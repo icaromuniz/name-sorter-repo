46487d98e8b24c93ba01f0d6c6745e2326df95aa
icaromuniz <icaro.muniz@gmail.com>
2019-01-27 21:37:48 +1000

    unused code cleaned

--- name-sorter-solution/name-sorter/Program.cs
+++ name-sorter-solution/name-sorter/Program.cs
@@ -18,23 +18,23 @@ namespace NameSorter
             const string outputFileName = "sorted-names-list.txt";
             DataHandler dataHandler = new DataHandler();
 
-            // reading data to array
+            // reading data from file to array
             string[] namesList = dataHandler.ReadData(args[0]);
 
-            // does sorting
+            // does the sorting
             var stopwatch = System.Diagnostics.Stopwatch.StartNew();
             new ArraySorter<string>().Sort(namesList);
             stopwatch.Stop();
 
+            // prints sorted list to screen
             Console.WriteLine("Sorting time: " + stopwatch.Elapsed + "\n");
-
-            // print sorted list to screen
             dataHandler.PrintToScreen(namesList);
 
+            // prints sorted list to output file
             Console.WriteLine("\nPress any key to write result to the file \"" +
                     outputFileName + "\" and close...");
-
             dataHandler.PrintToFile(namesList, outputFileName);
+
             Console.ReadKey();
         }
     }
